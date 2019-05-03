297d09afb68e5c5a1912d571d2ace16835dbfe6d
Rafael Cerri <rafaelbcerri@gmail.com>
2019-05-02 22:28:17 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final httpLink = HttpLink(
+      uri: 'http://localhost:8000/graphql/',
+    );
+
+    final client = ValueNotifier(
+      GraphQLClient(
+        cache: InMemoryCache(),
+        link: httpLink
+      )
+    );
+
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+          ),
+          home: Scaffold(
+            appBar: AppBar(title: Text("Things")),
+            body: Things(),
+          ),
+        )
+      )
+    );
+  }
+}
+
+class Things extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final getThings = """
+      query getThings {
+        things {
+          text
+        }
+      }
+    """;
+
+    return Query(
+      options: QueryOptions(
+        document: getThings,
+        pollInterval: 10,
+      ),
+      builder: (QueryResult result, { VoidCallback refetch }) {
+
+        if (result.loading) {
+          return Text('Carregando');
+        }
+        print(result.data);
+        final things = result.data['things'];
+
+        return ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemCount: things.length,
+          itemBuilder: (context, i) {
+            final thing = things[i];
+            return Text(thing['text']);
+          },
+        );
+      }
+    );
+  }
+}
